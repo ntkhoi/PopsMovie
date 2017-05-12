40f6647e5e343da143391cac9cfe25fa421bc1fc
Khoi Nguyen Trong <khoint@ITTeam.local>
2017-05-12 17:30:56 +0700

    Add list view to home movies

---
+++ apps/appstyle/decoration.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export default AppStyle = {
+    
+    movie_container: {
+        backgroundColor: 'orange',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    movie_cell: {
+        backgroundColor: 'red',
+        margin: 3,
+        width: 100,
+        alignItems: 'flex-start',
+    }
+
+};
--- apps/components/homescene.js
+++ apps/components/homescene.js
@@ -5,50 +5,63 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView
 } from 'react-native';
 
-
-
+import AppStyles from '../appstyle/decoration.js'
 
 export default class HomeScene extends Component{
+
+  constructor(){
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
   static navigationOptions = {
     title: 'Welcome',
   };
-    render(){
+
+   componentDidMount() {
+        this.fetchMovieDatas('https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed');
+        
+    }
+
+  fetchMovieDatas(uri){
+    return fetch(uri).then((response) => response.json()).then((responseJson) => {
+            console.log(responseJson);
+            this.setState({
+                dataSource: this
+                    .state
+                    .dataSource
+                    .cloneWithRows(responseJson.results), 
+            });
+
+        }).catch((error) => {
+            alert("Please check your network connection and try again later.");
+            console.error(error);
+        });
+  }
+
+  _renderRow(rowData){
+    return (
+         <Text style={AppStyles.movie_cell}> ABC XYSDASD </Text>
+    )
+  }
+  
+  render(){
     
         return(
-            <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+            <ListView style={AppStyles.movie_container}
+                dataSource={this.state.dataSource}
+                renderRow={this._renderRow}
+              />
         );
     }
+
+    
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
